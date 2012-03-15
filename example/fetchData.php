092d5fd5f0575418d12b91561af0314a5dc47a3f
FrancisVarga <github@varga-net.com>
2012-03-15 14:21:10 +0100

    add example

--- example/fetchData.php
+++ example/fetchData.php
@@ -7,20 +7,29 @@
  * To change this template use File | Settings | File Templates.
  */
 
+echo "=== Init === \n" . PHP_EOL;
+
 require "../src/Crowdpark/Net/JsonRpc/BaseHttpClient.php";
 require "../src/Crowdpark/Net/JsonRpc/InterfaceJsonRpcRequest.php";
 require "../src/Crowdpark/Net/JsonRpc/JsonRpcDataVo.php";
 require "../src/Crowdpark/Net/JsonRpc/Client.php";
 
+echo "after require \n" . PHP_EOL;
+
+$rpcMethod = "Open.User.getInitialData";
+$rpcParams = array();
+$rpcId     = 1;
+
 $vo = new \Crowdpark\Net\JsonRpc\JsonRpcDataVo();
-$vo->setMethod("Open.User.getInitialData")
-    ->setParams(array())
-    ->setRpcId(1);
+$vo->setMethod($rpcMethod)
+    ->setParams($rpcParams)
+    ->setRpcId($rpcId);
 
-var_dump($vo);
-die();
+echo "<pre>";
+echo var_dump($vo->getPostData());
+echo "</pre>";
 
-$jsonRpcClient = new Crowdpark\Net\JsonRpc\Client();
+$jsonRpcClient = new \Crowdpark\Net\JsonRpc\Client();
 $content       = $jsonRpcClient->setGateway("http://dev.shakeonitapp.com/api/v1/open/")
     ->addRequest($vo)
     ->addRequest($vo)
